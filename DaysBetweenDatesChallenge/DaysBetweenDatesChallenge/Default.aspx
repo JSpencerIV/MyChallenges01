17e3eeffa5855e525fca2d4959760bff393cde34
John Spencer IV <32145224+JSpencerIV@users.noreply.github.com>
2018-02-06 11:38:20 -0800

    Initial commit

---
+++ DaysBetweenDatesChallenge/DaysBetweenDatesChallenge/Default.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="DaysBetweenDatesChallenge.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            How many days have elapsed?<br />
+            <br />
+            Chose one date:<asp:Calendar ID="FirstCalendar" runat="server"></asp:Calendar>
+            <br />
+            <br />
+            Chose second date:<asp:Calendar ID="SecondCalendar" runat="server"></asp:Calendar>
+            <br />
+            <br />
+            <asp:Button ID="OKButton" runat="server" OnClick="OKButton_Click" Text="OK" />
+            <br />
+            <br />
+            <asp:Label ID="resultLabel" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ DaysBetweenDatesChallenge/DaysBetweenDatesChallenge/Default.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DaysBetweenDatesChallenge
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void OKButton_Click(object sender, EventArgs e)
+        {
+
+            if (FirstCalendar.SelectedDate > SecondCalendar.SelectedDate)
+            {
+                resultLabel.Text = FirstCalendar.SelectedDate
+                .Subtract(SecondCalendar.SelectedDate)
+                .TotalDays.ToString();
+            }
+            else
+            {
+                resultLabel.Text = SecondCalendar.SelectedDate
+                .Subtract(FirstCalendar.SelectedDate)
+                .TotalDays.ToString();
+            }
+            
+        }
+
+    }
+}
